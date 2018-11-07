a04972f080d6fe62b71e327d0d93129ae7ba1e91
Marcus Behel <marcus.behel@mindspring.com>
2018-11-07 17:29:56 -0500

    Added hardware interface project

---
+++ HP6050AInterface/Communicator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HP6050AInterface {
+    public class Communicator {
+
+        /// <summary>
+        /// Start communication with the tester
+        /// </summary>
+        /// <param name="port">Which port to open</param>
+        public void open(int port) {
+
+        }
+
+        /// <summary>
+        /// Start a certain test defined by an ITest object
+        /// </summary>
+        /// <param name="test">The test to start</param>
+        public void startTest(Tests test) {
+
+        }
+
+    }
+}
---
+++ HP6050AInterface/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HP6050AInterface {
+
+    /// <summary>
+    /// Basic test interface. Implement to create a test
+    /// </summary>
+    public interface ITest {
+        string getCommands();
+    }
+
+    public class BatteryTest : ITest {
+
+        // Properties specific to a battery test
+        double eodVoltage;
+        double cellCount;
+        double dischargeRate;
+
+        /// <summary>
+        /// Create a battery test with the given parameters
+        /// </summary>
+        /// <param name="eodVoltage">End of discharge voltage for each cell</param>
+        /// <param name="cellCount">The number of cells</param>
+        /// <param name="dischargeRate">Rate of discharge in amps</param>
+        public BatteryTest(double eodVoltage, double cellCount, double dischargeRate) {
+            this.eodVoltage = eodVoltage;
+            this.cellCount = cellCount;
+            this.dischargeRate = dischargeRate;
+        }
+
+        public string getCommands() {
+            StringBuilder builder = new StringBuilder();
+
+            // Set eodvoltage, cellcount, and dischargerate
+            builder.Append(String.Format("Eodv={0}", eodVoltage)); //TODO: append newline
+
+        }
+
+    }
+
+}
