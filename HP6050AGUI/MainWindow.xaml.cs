b9f72b203d3b6df1786038a8e37b4c368f589fe7
Marcus Behel <marcus.behel@mindspring.com>
2018-11-10 12:22:41 -0500

    Added battery testing method

--- HP6050AGUI/MainWindow.xaml.cs
+++ HP6050AGUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using HP6050AInterface;
+using NationalInstruments.Visa;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -21,18 +23,106 @@ namespace HP6050AGUI {
     /// </summary>
     public partial class MainWindow : Window {
 
+        struct DataPoint {
+            public DataPoint(long timeMs, double measuredVoltage, double measuredCurrent) {
+                this.timeMs = timeMs;
+                this.measuredVoltage = measuredVoltage;
+                this.measuredCurrent = measuredCurrent;
+            }
+            public long timeMs { get; private set; }
+            public double measuredVoltage { get; private set; }
+            public double measuredCurrent { get; private set; }
+        }
+
+        List<DataPoint> testResults = new List<DataPoint>();
+
+        string lastResourceString;
+        MessageBasedSession mbSession;
+
         public MainWindow() {
             InitializeComponent();
+            setControlState(false);
+        }
+
+        public void setControlState(bool isOpen) {
+            openSession.IsEnabled = !isOpen;
+            closeSession.IsEnabled = isOpen;
+            testSection.IsEnabled = isOpen;
         }
 
         private void openSession_Click(object sender, RoutedEventArgs e) {
             var d = new SelectResourceDialog();
             d.Owner = this;
-            d.ShowDialog();
+            if (lastResourceString != null)
+                d.ResourceName = lastResourceString;
+            bool? res = d.ShowDialog();
+            if (res == true) {
+                lastResourceString = d.ResourceName;
+                using(var rmSession = new ResourceManager()) {
+                    try {
+                        Console.WriteLine("Opening " + d.ResourceName);
+                        mbSession = (MessageBasedSession)rmSession.Open(d.ResourceName);
+                    } catch (InvalidCastException) {
+                        MessageBox.Show("Resource selected must be a message-based session");
+                    }catch(Exception err) {
+                        MessageBox.Show(err.Message);
+                    }
+                }
+            }
         }
 
         private void closeSession_Click(object sender, RoutedEventArgs e) {
+            setControlState(false);
+            mbSession.Dispose();
+        }
 
+
+        /// <summary>
+        /// Start a battery test using the same method as the script on page 3-6
+        /// </summary>
+        /// <param name="eodVoltage">End of discharge voltage for a single cell</param>
+        /// <param name="cellCount">The number of cells to be discharged in series</param>
+        /// <param name="dischargeRate">Constant current dicharge rate in amps</param>
+        public async Task startBatteryTest(double eodVoltage, int cellCount, double dischargeRate, long maxTimeMs = -1) {
+            await Task.Run(() => {
+                mbSession.RawIO.Write("INPUT OFF");
+                mbSession.RawIO.Write("MODE:CURRENT");
+                mbSession.RawIO.Write("CURRENT:LEVEL" + dischargeRate);
+                mbSession.RawIO.Write("INPUT ON");
+                // Start timing
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                double measuredVoltage = 0;
+                double measuredCurrent = 0;
+                bool shouldEnd = false;
+                do {
+                    try {
+                        // Read voltage
+                        mbSession.RawIO.Write("MEASURE:VOLTAGE?");
+                        measuredVoltage = Double.Parse(mbSession.RawIO.ReadString());
+                        // Read actual current
+                        mbSession.RawIO.Write("MEASURE:CURRENT?");
+                        measuredCurrent = Double.Parse(mbSession.RawIO.ReadString());
+
+                        // Add the data
+                        testResults.Add(new DataPoint(stopWatch.ElapsedMilliseconds, measuredVoltage, measuredCurrent));
+                    }catch (Exception e) {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    // Stop conditions
+                    bool timedOut = false;
+                    if (maxTimeMs > -1)
+                        timedOut = stopWatch.ElapsedMilliseconds >= maxTimeMs;
+                    shouldEnd = (measuredVoltage < cellCount * eodVoltage) || shouldEnd;
+                } while (!shouldEnd);
+                Console.WriteLine("Test done.");
+                stopWatch.Stop();
+            });
+        }
+
+        private void quickTestBtn_Click(object sender, RoutedEventArgs e) {
+            
         }
     }
 }
--- HP6050AGUI/SelectResourceDialog.xaml.cs
+++ HP6050AGUI/SelectResourceDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NationalInstruments.Visa;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,39 @@ namespace HP6050AGUI {
     public partial class SelectResourceDialog : Window {
         public SelectResourceDialog() {
             InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            using (var rmSession = new ResourceManager()) {
+                try {
+                    var resources = rmSession.Find("(ASRL|GPIB|TCPIP|USB)?*");
+                    foreach (string s in resources) {
+                        availableResourceList.Items.Add(s);
+                    }
+                } catch (Ivi.Visa.VisaException) {
+                    Console.WriteLine("No Supported devices found!");
+                }
+            }
+        }
+
+        private void availableResourceList_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            string selectedString = (string)availableResourceList.SelectedItem;
+            ResourceName = selectedString;
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        public string ResourceName {
+            get {
+                return resourceString.Text;
+            }
+            set {
+                resourceString.Text = value;
+            }
         }
     }
 }
